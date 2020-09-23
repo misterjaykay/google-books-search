cdc50ce2e5469e0f6e849605d9328a4b360ed4fb
Jonghyun Kang <misterjaykay@gmail.com>
2020-09-22 20:51:25 -0400

    navbar finished

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,5 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
-    return null
+    return (
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+            <Link className="navbar-brand" to="/">
+                Google Books
+            </Link>
+            <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                    <Link to="/search"
+                    className={
+                        window.location.pathname === "/" ||
+                        window.location.pathname === "/search" ? "nav-link active" : "nav-link"} >
+                        Search
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/home"
+                    className={
+                        window.location.pathname === "/saved" ? "nav-link active" : "nav-link"}>
+                        Saved
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    )
 }
